d451541d4b99882fc2bbde0a27cb3c97436552f8
hazel-heejeong-nam <hatbi2000@yonsei.ac.kr>
2025-12-22 16:09:22 -0500

    refactor

--- SlotFormer/slotformer/phyre_planning/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .readout import PHYREReadout
-
-
-def build_model(params):
-    """Build slot-based VQA model Aloe."""
-    assert params.model == 'PHYREReadout', f'Unknown model: {params.model}'
-    model = PHYREReadout(readout_dict=params.readout_dict)
-    return model
